921d41c2ba9acdc9866ffe7d45f6f7b63230bb24
Emily Miles <emilymiles193@gmail.com>
2023-04-25 16:21:16 -0400

    completed the logo maker

--- index.js
+++ index.js
@@ -39,18 +39,19 @@ function createFile(name, data){
 function initializeShapes(data){
 if(data.shape=='Circle'){
     const newCircle= new Circle(data.characters, data.color, data.shapeColor);
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${circle.render()}<text x="150" y="125" font-size="60" 
-    text-anchor="middle" fill="${circle.color}">${circle.characters}</text></svg>`;
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${newCircle.render()}<text x="150" y="125" font-size="60" 
+    text-anchor="middle" fill="${newCircle.color}">${newCircle.characters}</text></svg>`;
 }
 else if(data.shape=='Triangle'){
     const newTriangle= new Triangle(data.characters, data.color, data.shapeColor);
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${square.render()}<text x="150" y="125" font-size="60" 
-    text-anchor="middle" fill="${square.color}">${square.characters}</text></svg>`;
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${newTriangle.render()}<text x="150" y="125" font-size="60" 
+    text-anchor="middle" fill="${newTriangle.color}">${newTriangle.characters}</text></svg>`
+    
 }
 else if(data.shape=='Square'){
     const newSquare= new Square(data.characters, data.color, data.shapeColor);
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${triangle.render()}<text x="150" y="125" font-size="60" 
-    text-anchor="middle" fill="${triangle.color}">${triangle.characters}</text></svg>`
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${newSquare.render()}<text x="150" y="125" font-size="60" 
+    text-anchor="middle" fill="${newSquare.color}">${newSquare.characters}</text></svg>`;
 }
 else{
     console.log("Error");
